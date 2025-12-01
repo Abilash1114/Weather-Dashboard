077a12dd23b48622fe30884ba85a03352224d692
Abilash Ravi <abilashravi09@gmail.com>
2025-12-01 10:35:48 +0530

    morning_update

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -1,12 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+dailyUpdate=[
+  {
+    day:'SAT',temp:'21',icon:'assets/images/sun.png'
+  },
+  {
+day:'SUN',temp:'21',icon:'assets/images/sun.png'
+  },
+  {
+    day:'MON',temp:'21',icon:'assets/images/sun.png'
+  },
+  {
+    day:'TUE',temp:'21',icon:'assets/images/sun.png'
+  },
+  {
+    day:'WEN',temp:'21',icon:'assets/images/sun.png'
+  },
+  {
+    day:'THU',temp:'21',icon:'assets/images/sun.png'
+  }
+]
 
 }
